8d440f8bf103fae6efce226a8cb44fbe5cd5370d
sabbir-anwar <protik26@gmail.com>
2020-11-21 18:59:55 +0600

    added all images and svg

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,16 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import logo from "../svg/logo.svg";
 
 class Header extends Component {
   render() {
     return (
-      <div>
-        <h1>Header</h1>
+      <div className="header-container">
+        <div className="header-top">
+          <img src={logo} alt="logo" />
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default  Header;
+export default Header;
