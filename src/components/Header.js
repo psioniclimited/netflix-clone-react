3a786acdab08fd27650d058708467b25e138fed6
sabbir-anwar <protik26@gmail.com>
2020-11-21 19:34:18 +0600

    Added styled-component || home page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,55 @@
 import React, { Component } from "react";
 import logo from "../svg/logo.svg";
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
 
 class Header extends Component {
   render() {
     return (
-      <div className="header-container">
+      <HeaderComponent className="header-container">
         <div className="header-top">
-          <img src={logo} alt="logo" />
+          <Logo src={logo} alt="logo" />
+          {/* <NavLink className="signIn-btn">Sign In</NavLink> */}
         </div>
-      </div>
+      </HeaderComponent>
     );
   }
 }
 
 export default Header;
+
+// logo
+const Logo = styled.img`
+  width: 10rem;
+  height: 3.5rem;
+  position: absolute;
+  top: 25%;
+  left: 25%;
+  transform: translate(-50%, -50%);
+`;
+
+// Header Container
+const HeaderComponent = styled.div`
+  .signIn-btn {
+    right: 0;
+    margin: 1.125rem 3% 0;
+    padding: 0.4375rem 1.0625rem;
+    font-weight: 400;
+    line-height: normal;
+    border-radius: 0.1875rem;
+    background: var(--main-red);
+    position: absolute;
+    translate: transform(-50%, -50%);
+    cursor: pointer;
+    transition: background 0.2s ease-in;
+    &:hover {
+      background: var(--main-red-hover);
+    }
+  }
+
+  .header-top {
+    position: relative;
+    height: 10rem;
+    z-index: 1;
+  }
+`;
