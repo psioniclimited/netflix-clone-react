b440324a4d37bc50da9753ad6ed4608e6376a85b
sabbir-anwar <protik26@gmail.com>
2020-11-23 16:20:34 +0600

    home page styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import logo from "../svg/logo.svg";
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 import styled from "styled-components";
 
 class Header extends Component {
@@ -10,6 +10,12 @@ class Header extends Component {
         <div className="header-top">
           <Logo src={logo} alt="logo" />
           {/* <NavLink className="signIn-btn">Sign In</NavLink> */}
+        </div>
+        {/* Header Content */}
+        <div className="header-content">
+          <Title>See what's next</Title>
+          <SubTitle>WATCH ANYWHERE. CANCEL ANYTIME</SubTitle>
+          <Link className="main-offer-btn">try it now</Link>
         </div>
       </HeaderComponent>
     );
@@ -24,7 +30,7 @@ const Logo = styled.img`
   height: 3.5rem;
   position: absolute;
   top: 25%;
-  left: 25%;
+  left: 50%;
   transform: translate(-50%, -50%);
 `;
 
@@ -47,9 +53,59 @@ const HeaderComponent = styled.div`
     }
   }
 
+  // Header Top
   .header-top {
     position: relative;
     height: 10rem;
     z-index: 1;
   }
+
+  // Header Content
+  .header-content{
+    width: 65%;
+    position: relative;
+    margin: 4.5rem auto 0;
+    display: flex;
+    justify-content: center;
+    align-content: center;
+    text-align: center;
+    flex-direction: column;
+    z-index: 1;
+  }
+
+  .main-offer-btn{
+    display: inline-block;
+    background: var(--main-red);
+    text-transform: uppercase;
+    border: none;
+    outline: none;
+    margin: 0 33%;
+    padding: 1.5rem;
+    border-radius: 0.1875rem;
+    font-size: 2rem;
+    text-align: center;
+    box-shadow: 0 1px 0 rgba(0,0,0,0.45);
+    transition: background 0.2s ease-in;
+    cursor: pointer;
+    &:hover{
+      background: var(--main-red-hover);
+    }
+  }
+`;
+
+// Main title
+const Title = styled.h1`
+  margin: 0 0 1.2rem;
+  font-size: 5rem;
+  font-weight: 700;
+  line-height: 1.1rem;
+`;
+
+// Subtitle
+const SubTitle = styled.h2`
+  font-weight: 400;
+  font-size: 1.625rem;
+  line-height: 1.25rem;
+  margin: 1rem auto;
+  text-transform: uppercase;
 `;
